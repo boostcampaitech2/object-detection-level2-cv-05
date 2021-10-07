ccd4c0a6500563147af80210f371b0b18ba1f164
lkm2835 <lkm2835@gmail.com>
2021-10-07 11:01:38 +0900

    lkm configs

---
+++ configs/lkm/configs/_base_/default_runtime.py
@@ -0,0 +1,21 @@
+checkpoint_config = dict(max_keep_ckpts=2, interval=1)
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        #dict(type='TextLoggerHook')
+        dict(type='MyHook', early_stopping=5),
+    ])
+# yapf:enable
+custom_hooks = [dict(type='NumClassCheckHook')]
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+
+gpu_ids = [0]
+seed = 2147
+
+evaluation = dict(interval=1, metric='bbox', save_best='bbox_mAP_50')
