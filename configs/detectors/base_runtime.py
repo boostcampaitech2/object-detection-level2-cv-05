0a809fa67634dfd363923fa8595a97883c95df59
wonsgong <42221106+wonsgong@users.noreply.github.com>
2021-10-17 14:01:44 +0900

    update detectors

---
+++ configs/detectors/base_runtime.py
@@ -0,0 +1,21 @@
+checkpoint_config = dict(max_keep_ckpts=3, interval=1)
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='MyHook')]
+        # dict(type='TextLoggerHook')]
+)
+
+# yapf:enable
+custom_hooks = [dict(type='NumClassCheckHook')]
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+
+gpu_ids = [0]
+seed = 830
+
